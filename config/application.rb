5ec7ee1f705c125ce9d47f7e86e555ea6d6e4245
yuhki1682 <yuki1682@gmail.com>
2024-10-18 23:56:07 +0900

    画像表示の実装

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,7 @@ module ChatApp
     config.i18n.default_locale = :ja
     config.time_zone = 'Tokyo'
     config.active_storage.variant_processor = :mini_magick
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
